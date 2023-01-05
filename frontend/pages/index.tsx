2352f7535188d1d1d5daed8a76ffd070ad77d7a2
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-01-05 19:46:46 +0200

    Add prettier (#5)
    * Add prettier
    
    * Fix
    
    * Fix 2
    
    * Fix 3
    
    * Fix 4

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -20,38 +20,54 @@ export default function Home() {
                   <p>Next steps:</p>
                   <ul className="space-y-4">
                     <li className="flex items-center">
-                      <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
+                      <svg
+                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
                         <circle cx="12" cy="12" r="11" />
                         <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
                       </svg>
                       <p className="ml-4">
-                      Start by editing&nbsp;
+                        Start by editing&nbsp;
                         <code className="text-sm font-bold text-gray-900">pages/index.tsx</code>&nbsp;file
                       </p>
                     </li>
 
                     <li className="flex items-center">
-                      <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
+                      <svg
+                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
                         <circle cx="12" cy="12" r="11" />
                         <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
                       </svg>
                       <p className="ml-4">
-                      Customizing your&nbsp;
+                        Customizing your&nbsp;
                         <code className="text-sm font-bold text-gray-900">tailwind.config.js</code>&nbsp;file
                       </p>
                     </li>
                     <li className="flex items-center">
-                      <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
+                      <svg
+                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
                         <circle cx="12" cy="12" r="11" />
                         <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
                       </svg>
                       <p className="ml-4">
-                      Extracting classes with&nbsp;
+                        Extracting classes with&nbsp;
                         <code className="text-sm font-bold text-gray-900">@apply</code>
                       </p>
                     </li>
                     <li className="flex items-center">
-                      <svg className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2" strokeLinecap="round" strokeLinejoin="round">
+                      <svg
+                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                      >
                         <circle cx="12" cy="12" r="11" />
                         <path d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9" fill="none" />
                       </svg>
@@ -64,12 +80,19 @@ export default function Home() {
                 <div className="pt-8 text-base font-semibold leading-7">
                   <p className="text-gray-900">Find in-depth information about Next.js features and&nbsp;API.</p>
                   <p>
-                    <a href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app" className="text-sky-500 hover:text-sky-600">Read the docs &rarr;</a>
+                    <a
+                      href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+                      className="text-sky-500 hover:text-sky-600"
+                    >
+                      Read the docs &rarr;
+                    </a>
                   </p>
 
                   <p className="text-gray-900">Want to dig deeper into Tailwind?</p>
                   <p>
-                    <a href="https://tailwindcss.com/docs" className="text-sky-500 hover:text-sky-600">Read the docs &rarr;</a>
+                    <a href="https://tailwindcss.com/docs" className="text-sky-500 hover:text-sky-600">
+                      Read the docs &rarr;
+                    </a>
                   </p>
                 </div>
               </div>
